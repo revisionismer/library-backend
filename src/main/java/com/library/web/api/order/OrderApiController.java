ee28786059ddec18e457dbf82bbe126609568c1e
revisionismer <cherry2137@naver.com>
2025-04-18 11:33:20 +0900

    15. 주문 API 서비스, 장바구니 API 서비스 수정

--- src/main/java/com/library/web/api/order/OrderApiController.java
+++ src/main/java/com/library/web/api/order/OrderApiController.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.library.config.auth.PrincipalDetails;
 import com.library.domain.user.User;
 import com.library.service.order.OrderService;
 import com.library.web.dto.ResponseDto;
 import com.library.web.dto.cart.CartItemListRespDto;
+import com.library.web.dto.order.OrderRespDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,12 +29,22 @@ public class OrderApiController {
 	private final OrderService orderService;
 	
 	@PostMapping("/s/cartItem")
-	public ResponseEntity<?> orderCartItems(@RequestBody String jsonString, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+	public ResponseEntity<?> sendCartItems(@RequestBody String jsonString, @AuthenticationPrincipal PrincipalDetails principalDetails) {
 		
 		User loginUser = principalDetails.getUser();
 		
-		CartItemListRespDto result = orderService.orderCartItem(jsonString, loginUser.getId());
+		CartItemListRespDto result = orderService.conveyCartItems(jsonString, loginUser.getId());
 		
-		return new ResponseEntity<>(new ResponseDto<>(1, loginUser.getId() + "번 유저 장바구니 아이템 주문 하기", result), HttpStatus.OK);
+		return new ResponseEntity<>(new ResponseDto<>(1, loginUser.getId() + "번 유저 장바구니 아이템 정보 불러오기", result), HttpStatus.OK);
 	}
+	
+	@PostMapping("/s/order")
+	public ResponseEntity<?> orderItems(@RequestBody String jsonString, @AuthenticationPrincipal PrincipalDetails principalDetails) throws JsonMappingException, JsonProcessingException {
+		User loginUser = principalDetails.getUser();
+		
+		OrderRespDto orderRespDto = orderService.orderItems(jsonString, loginUser.getId());
+		
+		return new ResponseEntity<>(new ResponseDto<>(1, loginUser.getId() + "번 유저 아이템 주문 하기", orderRespDto), HttpStatus.OK);
+	}
+	
 }
