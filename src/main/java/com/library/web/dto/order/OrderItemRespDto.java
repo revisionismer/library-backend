69614d8dfcaee3c08501472969db8eb24b87f42f
revisionismer <cherry2137@naver.com>
2025-05-25 16:01:08 +0900

    17. 주문 정보 API 서비스 구현(미완성)

---
+++ src/main/java/com/library/web/dto/order/OrderItemRespDto.java
@@ -0,0 +1,37 @@
+package com.library.web.dto.order;
+
+import java.math.BigDecimal;
+
+import com.library.domain.order_item.OrderItem;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+@NoArgsConstructor
+public class OrderItemRespDto {
+
+	private Long orderId;
+	private Long orderItemId;
+	
+	private String itemNm;
+	private BigDecimal price;
+	private int count;
+	private Long unitsInStock;
+	
+	private BigDecimal totalOrderPrice;
+	
+	public OrderItemRespDto(OrderItem orderItem) {
+		this.orderId = orderItem.getOrder().getId();
+		this.orderItemId = orderItem.getId();
+		this.itemNm = orderItem.getBook().getName();
+		this.price = orderItem.getBook().getUnitPrice();
+		this.count = orderItem.getQuantity();
+		this.unitsInStock = orderItem.getBook().getUnitsInStock();
+		this.totalOrderPrice = orderItem.getTotalPrice();
+		
+	}
+}
