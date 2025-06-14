087ca5f2e21ce9586ffbbd43ea6bf7d2380deef7
revisionismer <cherry2137@naver.com>
2025-06-14 17:52:53 +0900

    21. OrderListPageRespDto 코드 수정

--- src/main/java/com/library/web/dto/order/OrderInfoRespDto.java
+++ src/main/java/com/library/web/dto/order/OrderInfoRespDto.java
@@ -1,9 +1,6 @@
 package com.library.web.dto.order;
 
-import java.math.BigDecimal;
-
 import com.library.domain.order.Order;
-import com.library.domain.order_item.OrderItem;
 import com.library.domain.user.User;
 
 import lombok.Getter;
@@ -15,19 +12,13 @@ import lombok.ToString;
 public class OrderInfoRespDto {
 	
 	private Long orderId;
-	private Long orderItemId;
 	private Long customerId;
 	private String deliveryAddress;
-	private String thumnailImageUrl;
-	private BigDecimal totalOrderPrice;
 	
-	public OrderInfoRespDto(Order order, OrderItem orderItem, User user, String deliveryAddress, BigDecimal totalOrderPrice) {
+	public OrderInfoRespDto(Order order, User user, String deliveryAddress) {
 		this.orderId = order.getId();
-		this.orderItemId = orderItem.getId();
 		this.customerId = user.getId();
 		this.deliveryAddress = deliveryAddress;
-		this.thumnailImageUrl = orderItem.getBook().getBookImageUrl();
-		this.totalOrderPrice = totalOrderPrice;
 	}
 	
 }
